c3e8e71dcd9b3222bfc840b45fca3ecba0ae2e3f
venda <32082967+jitaiyx@users.noreply.github.com>
2018-11-27 14:58:22 +0800

    1127
    start

---
+++ main.js
@@ -0,0 +1,14 @@
+new Vue({
+    el:"#app",
+    data:{
+      list:[
+        {name:"挂号前需要收集的资料有哪些？",adr:"q1.html"},
+        {name:"没有北京医保卡的客人要提供？",adr:"q2.html"},
+        {name:"外地的医保卡北京不能用，提醒客人就诊当天带齐？",adr:"q3.html"},
+        {name:"北京就医流程？",adr:"q4.html"},
+        {name:"科室如何报到？",adr:"q5.html"},
+        {name:"如何排队就诊？",adr:"q6.html"},
+        {name:"挂号的注意事项有哪些？",adr:"q7.html"}
+      ]
+    }
+})
